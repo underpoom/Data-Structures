6a6078986c2cadf041cb7983be01af4d38d2363d
0xPoom <37230567+underpoom@users.noreply.github.com>
2021-08-13 02:13:50 +0700

    non

--- Grader_1/multiplication or sum.py
+++ Grader_1/multiplication or sum.py
@@ -1,3 +1,3 @@
 print('*** multiplication or sum ***')
-a,b = map(int,input('nEnter num1 num2 : ').split())
+a,b = (int(e) for e in input('nEnter num1 num2 : ').split())
 print("The result is",a+b)  if(a*b>1000) else  print("The result is",a*b)
