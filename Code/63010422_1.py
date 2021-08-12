80714bc2def28d4c0e9ccc21b61bafa6b206b6ac
0xPoom <37230567+underpoom@users.noreply.github.com>
2021-08-13 01:25:04 +0700

    1

---
+++ Code/63010422_1.py
@@ -0,0 +1,4 @@
+r = float(input("r="))
+a = r*r*3.141592653589793
+print("Area=",end="")
+print(a)
---
+++ Code/E.py
@@ -0,0 +1,8 @@
+print('*** Minesweeper ***')
+s = input('Enter input(5x5) : ').split(',')
+
+print('\n')
+
+
+s[0] = s[0].split()
+print(s[0])
