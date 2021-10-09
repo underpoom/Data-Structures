ef3f942c867cb4804bfe04ed96ee299ede0140e4
0xPoom <37230567+underpoom@users.noreply.github.com>
2021-10-09 20:15:01 +0700

    non

---
+++ nQueensProblem/nQueensProblem (BruteForce).py
@@ -0,0 +1,54 @@
+from itertools import permutations
+import timeit
+N = 9
+
+start = timeit.default_timer()
+from itertools import permutations
+
+def print_table():
+    for row in range(N):
+        print(table[row])
+
+def put_queen(x,y):
+    if table[y][x] == 0:
+        for m in range(N):
+            table[y][m] = 1
+            table[m][x] = 1
+            table[y][x] = 2
+            if y+m <= N-1 and x+m <= N-1:
+                table[y+m][x+m] = 1
+            if y-m >= 0 and x+m <= N-1:
+                table[y-m][x+m] = 1
+            if y+m <= N-1 and x-m >= 0:
+                table[y+m][x-m] = 1
+            if y-m >= 0 and x-m >= 0:
+                table[y-m][x-m] = 1
+        return True
+    else:
+        return False
+
+table = [[0]*N for _ in range(N)]    
+perms = permutations([i for i in range(N)])
+
+num_comb = 0
+k = 0
+for perm in perms:
+    for i in range(N):
+        if put_queen(perm[i], i) == True:
+            k+=1
+        else:
+            k=0
+            break
+    if k == N:
+        k=0
+        print_table()
+        num_comb += 1
+        print(f"solution{num_comb}")
+        print(" ")
+    
+                                   
+    table = [[0] * N for _ in range(N)]
+
+stop = timeit.default_timer()
+
+print('Time: ', stop - start)  #Timer
