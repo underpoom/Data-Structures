84c0e9d4e09cb44d48bbe6854102259181ec0e5e
0xPoom <37230567+underpoom@users.noreply.github.com>
2021-11-12 14:09:25 +0700

    non

--- Grader/Searching/Fun with hashing.py
+++ Grader/Searching/Fun with hashing.py
@@ -2,10 +2,8 @@ class Data:
     def __init__(self, key, value):
         self.key = key
         self.value = value
-
     def __str__(self):
         return "({0}, {1})".format(self.key, self.value)
-
 class hash:
     def __init__(self,data,sTable):
         data = data.split()
